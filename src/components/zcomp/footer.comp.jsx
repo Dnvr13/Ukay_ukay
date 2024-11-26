6b0d1b2b14718706cefe70c4740d81c4ce03038c
Dnvr13 <denver.sacragon@hcdc.edu.ph>
2024-11-26 23:17:59 +0800

    change bg

--- src/components/zcomp/footer.comp.jsx
+++ src/components/zcomp/footer.comp.jsx
@@ -34,7 +34,7 @@ const SocialMediaLinks = () => {
 // Main Footer Component
 const FooterComp = () => {
   return (
-    <footer className="bg-white py-6">
+    <footer className="bg-gray-800 py-6 text-white">
       <div className="max-w-screen-xl mx-auto px-5 flex flex-col md:flex-row justify-between">
         {/* About Section */}
         <div className="md:w-1/2 mb-6 md:mb-0">
@@ -51,15 +51,21 @@ const FooterComp = () => {
             <h4 className="text-sm font-bold">Gcash</h4>
             <p className="text-xs font-light">+639501127321</p>
           </div>
-          <div className="bg-white rounded-full p-5 text-3xl font-light">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-qr-code w-32 h-32" viewBox="0 0 16 16"> <path d="M2 2h2v2H2V2Z" /> <path d="M6 0v6H0V0h6ZM5 1H1v4h4V1ZM4 12H2v2h2v-2Z" /> <path d="M6 10v6H0v-6h6Zm-5 1v4h4v-4H1Zm11-9h2v2h-2V2Z" /> <path d="M10 0v6h6V0h-6Zm5 1v4h-4V1h4ZM8 1V0h1v2H8v2H7V1h1Zm0 5V4h1v2H8ZM6 8V7h1V6h1v2h1V7h5v1h-4v1H7V8H6Zm0 0v1H2V8H1v1H0V7h3v1h3Zm10 1h-1V7h1v2Zm-1 0h-1v2h2v-1h-1V9Zm-4 0h2v1h-1v1h-1V9Zm2 3v-1h-1v1h-1v1H9v1h3v-2h1Zm0 0h3v1h-2v1h-1v-2Zm-4-1v1h1v-2H7v1h2Z" /> <path d="M7 12h1v3h4v1H7v-4Zm9 2v2h-3v-1h2v-1h1Z" /> </svg>
+          <div className="bg-gray-700 rounded-full p-5 text-3xl font-light">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-qr-code w-32 h-32 text-white" viewBox="0 0 16 16"> 
+              <path d="M2 2h2v2H2V2Z" /> 
+              <path d="M6 0v6H0V0h6ZM5 1H1v4h4V1ZM4 12H2v2h2v-2Z" /> 
+              <path d="M6 10v6H0v-6h6Zm-5 1v4h4v-4H1Zm11-9h2v2h-2V2Z" /> 
+              <path d="M10 0v6h6V0h-6Zm5 1v4h-4V1h4ZM8 1V0h1v2H8v2H7V1h1Zm0 5V4h1v2H8ZM6 8V7h1V6h1v2h1V7h5v1h-4v1H7V8H6Zm0 0v1H2V8H1v1H0V7h3v1h3Zm10 1h-1V7h1v2Zm-1 0h-1v2h2v-1h-1V9Zm-4 0h2v1h-1v1h-1V9Zm2 3v-1h-1v1h-1v1H9v1h3v-2h1Zm0 0h3v1h-2v1h-1v-2Zm-4-1v1h1v-2H7v1h2Z" /> 
+              <path d="M7 12h1v3h4v1H7v-4Zm9 2v2h-3v-1h2v-1h1Z" /> 
+            </svg>
           </div>
         </div>
       </div>
 
       {/* Footer Bottom Section */}
-      <div className="text-center text-sm font-light mt-5 pt-3 border-t border-black">
-        © 2024 | Gulmatico 2nd Hand Clothing Store | All Rights Reserved | Powered by [Ukay-ukay.vercel].app
+      <div className="text-center text-sm font-light mt-5 pt-3 border-t border-gray-600">
+        Â© 2024 | Gulmatico 2nd Hand Clothing Store | All Rights Reserved | Powered by [Ukay-ukay.vercel].app
       </div>
     </footer>
   );
--- src/components/zcomp/header.comp.jsx
+++ src/components/zcomp/header.comp.jsx
@@ -13,33 +13,40 @@ const HeaderComp = () => {
   const isActive = (path) => location.pathname === path;
 
   return (
-    <header className="bg-white flex flex-col md:flex-row items-center justify-between p-5 md:p-6 lg:p-8 shadow-sm">
+    <header className="bg-gray-800 flex flex-col md:flex-row items-center justify-between p-5 md:p-6 lg:p-8 shadow-sm text-white">
       {/* Logo */}
       <img
         src="https://cdn.builder.io/api/v1/image/assets/TEMP/c6d0a7a6f1e9238ee939c4890c892dfaf179c0c3228dd98cc1712dad2439eb75?placeholderIfAbsent=true&apiKey=63cef383af9641cc969e43f7e6acc6c2"
         alt="Ukay-Ukay Online Shop Logo"
-        className="aspect-ratio[1.02] object-contain w-[172px] rounded-full max-w-full mb-4 md:mb-0"
-      onClick={()=>handleNavigation("/")}/>
+        className="aspect-ratio[1.02] object-contain w-[172px] rounded-full max-w-full mb-4 md:mb-0 cursor-pointer"
+        onClick={() => handleNavigation("/")}
+      />
 
       {/* Navigation */}
-      <nav className="flex items-center gap-10 text-black text-xl md:gap-8 mb-4 md:mb-0">
+      <nav className="flex items-center gap-10 text-xl md:gap-8 mb-4 md:mb-0">
         <button
           onClick={() => handleNavigation("/")}
-          className={`no-underline ${isActive("/") || isActive("/home") ? "font-bold border-b-2 border-light-blue-500 text-light-blue-700" : ""} hover:underline`}
+          className={`no-underline ${
+            isActive("/") || isActive("/home") ? "font-bold border-b-2 border-light-blue-500 text-light-blue-400" : ""
+          } hover:underline`}
         >
           Home
         </button>
         <div className="flex flex-col items-center">
           <button
             onClick={() => handleNavigation("/items")}
-            className={`no-underline ${isActive("/items") ? "font-bold border-b-2 border-light-blue-500 text-light-blue-700" : ""} hover:underline`}
+            className={`no-underline ${
+              isActive("/items") ? "font-bold border-b-2 border-light-blue-500 text-light-blue-400" : ""
+            } hover:underline`}
           >
             Items
           </button>
         </div>
         <button
           onClick={() => handleNavigation("/favorites")}
-          className={`no-underline ${isActive("/favorites") ? "font-bold border-b-2 border-light-blue-500 text-light-blue-700" : ""} hover:underline`}
+          className={`no-underline ${
+            isActive("/favorites") ? "font-bold border-b-2 border-light-blue-500 text-light-blue-400" : ""
+          } hover:underline`}
         >
           Favorites
         </button>
@@ -48,7 +55,14 @@ const HeaderComp = () => {
           className="bg-none border-none cursor-pointer p-0"
           aria-label="Notifications"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className={`w-8 h-8 text-gray-700 ${isActive("/cart") && "font-bold border-b-2 border-light-blue-500 text-light-blue-700"}`}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="currentColor"
+            className={`w-8 h-8 ${isActive("/cart") && "font-bold border-b-2 border-light-blue-500 text-light-blue-400"}`}
+          >
             <path strokeLinecap="round" strokeLinejoin="round" d="M3 3h18l-1.5 9H5.25L4.5 6H3zM16.5 21a1.5 1.5 0 100-3 1.5 1.5 0 000 3zm-9 0a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
           </svg>
         </button>
@@ -57,24 +71,43 @@ const HeaderComp = () => {
           className="bg-none border-none cursor-pointer p-0"
           aria-label="User Profile"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className={`w-8 h-8 text-gray-700 ${isActive("/profile") && "font-bold border-b-2 border-light-blue-500 text-light-blue-700"}`} viewBox="0 0 16 16">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className={`w-8 h-8 ${isActive("/profile") && "font-bold border-b-2 border-light-blue-500 text-light-blue-400"}`}
+            viewBox="0 0 16 16"
+          >
             <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-            <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+            <path
+              fillRule="evenodd"
+              d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
+            />
           </svg>
         </button>
       </nav>
 
       {/* Search Bar */}
-      <div className="flex items-center bg-white rounded-xl p-2 w-full max-w-[441px] shadow-lg">
-        <label htmlFor="searchInput" className="sr-only">Search</label>
+      <div className="flex items-center bg-gray-700 rounded-xl p-2 w-full max-w-[441px] shadow-lg">
+        <label htmlFor="searchInput" className="sr-only">
+          Search
+        </label>
         <input
           type="search"
           id="searchInput"
           placeholder="Search here"
-          className="flex-grow border-none text-base font-sans bg-transparent placeholder-black"
+          className="flex-grow border-none text-base font-sans bg-transparent placeholder-white text-white"
         />
-        <button type="submit" className="bg-none border-none cursor-pointer p-0" aria-label="Search">        
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-[26px] aspect-ratio[0.81] object-contain" viewBox="0 0 16 16">
+        <button type="submit" className="bg-none border-none cursor-pointer p-0" aria-label="Search">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="w-[26px] aspect-ratio[0.81] object-contain"
+            viewBox="0 0 16 16"
+          >
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
           </svg>
         </button>
